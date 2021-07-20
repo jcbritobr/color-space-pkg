4ccb5a71a845d80eb0457df02eb05ee6b1dd03fc
jcbritobr <jcbritobr@gmail.com>
2021-07-20 14:36:28 -0300

    first commit

---
+++ colorpkg/color.pas
@@ -0,0 +1,21 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit color;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  HSV, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('color', @Register);
+end.
